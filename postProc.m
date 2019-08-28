b6ed325607d244131946c89c2dceea4ece073bc6
Evgenii Vetchanin <veugene186@gmail.com>
2019-08-28 10:06:22 +0400

    Corrected data representation

--- postProc.m
+++ postProc.m
@@ -19,8 +19,8 @@ fprintf(1, "D in [%.15e, %.15e]\n", min(min(D)), max(max(D)));
 
 %contour(oo, ee, TR, [2 2]);
 
-%contour(oo, ee, TR, [2 2]);
-surfc(oo, ee, TR, 'edgecolor', 'none');
+contour(oo, ee, TR, [2 2]);
+%surfc(oo, ee, TR, 'edgecolor', 'none');
 zlim([0 10]);
 return
 eps = 1;
