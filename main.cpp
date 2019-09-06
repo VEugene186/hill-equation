6e91242ae3d5551461593ebe955e43e55d90d5bf
Evgenii Vetchanin <veugene186@gmail.com>
2019-09-06 11:54:50 +0400

    Optimization and generalization

--- main.cpp
+++ main.cpp
@@ -20,45 +20,13 @@ void startMenuLoop();
 void multitreadingOptions();
 void clearStream();
 
-void case1();
-void case2();
-void case3();
-void case4();
+void MathieuEquation_Arnold();
+void MathieuEquation_Kovacic();
+void EulerPoinsotStability();
 
 int main(int argc, char * argv[]) {
     startMenuLoop();
     
-    return 0;
-
-    printf("Number of threads: ");
-    int nThreads = 1;
-    scanf("%d", &nThreads);
-    omp_set_num_threads(nThreads);
-
-    printf("1 - Mathieu (Arnold)\n"
-           "2 - Mathieu (Kovacic)\n"
-           "3 - LiouvillHill\n"
-           "4 - LiouvillHill2\n");
-    int selection = -1;
-    scanf("%d", &selection);
-    switch (selection) {
-    case 1:
-        case1();
-        break;
-    case 2:
-        case2();
-        break;
-    case 3:
-        case3();
-        break;
-    case 4:
-        case4();
-        break;
-    default:
-        printf("Unknown selection\n");
-    }
-
-
     return 0;
 }
 
@@ -76,7 +44,7 @@ void multitreadingOptions() {
 void startMenuLoop() {
     vector<string> items;
     items.push_back("1 - x\'\' + omega^2 * (1 + a * cos(t)) * x = 0");
-    items.push_back("2 - x\'\' + (k + delta * cos(t)) * x = 0");
+    items.push_back("2 - x\'\' + (delta + epsilon * cos(t)) * x = 0");
     items.push_back("3 - Periodic perturbation of Euler-Poinsot case");
     items.push_back("4 - Periodic perturbation of Joukowskii-Volterra case");
     items.push_back("5 - Multi-threading options");
@@ -99,13 +67,13 @@ void startMenuLoop() {
         clearStream();
         switch (selected) {
         case 1:
-
+            MathieuEquation_Arnold();
             break;
         case 2:
-
+            MathieuEquation_Kovacic();
             break;
         case 3:
-
+            EulerPoinsotStability();
             break;
         case 4:
 
@@ -140,33 +108,62 @@ void saveToFile(const char * fName, valarray<double> & x, valarray<double> & y,
     fclose(f);
 }
 
-void case1() {
-    double omega_min = 0.0;
-    double omega_max = 2.5;
-    double e_min = 0.0;
-    double e_max = 2.0;
+void inputMesh(const char * par1Name, const char * par2Name, int * par1N, double * par1min, double * par1max, 
+                                                             int * par2N, double * par2min, double * par2max) {
+    printf("%s, nodes: ", par1Name);
+    scanf("%d", par1N);
+    printf("%s, min  : ", par1Name);
+    scanf("%lf", par1min);
+    printf("%s, max  : ", par1Name);
+    scanf("%lf", par1max);
 
-    int N_omega = 1001;
-    int N_e = 1001;
-    
-    double dOmega = (omega_max - omega_min) / (N_omega - 1);
-    double de = (e_max - e_min) / (N_e - 1);
+    printf("%s, nodes: ", par2Name);
+    scanf("%d", par2N);
+    printf("%s, min  : ", par2Name);
+    scanf("%lf", par2min);
+    printf("%s, max  : ", par2Name);
+    scanf("%lf", par2max);
+}
 
-    valarray<double> e(N_e);
-    valarray<double> omega(N_omega);
-    valarray<valarray<double> > tr(N_omega), det(N_omega);
-    for (int i = 0; i < N_omega; i++) {
-        tr[i].resize(N_e, 0.0);
-        det[i].resize(N_omega, 0.0);
+void createMesh(int N, double left, double right, valarray<double> & mesh, bool logScale = false) {
+    double step;
+    mesh.resize(N);
+    if (logScale) {
+        double p1 = log10(left);
+        double p2 = log10(right);
+        step = (p2 - p1) / (N - 1);
+        for (int i = 0; i < N; i++)
+            mesh[i] = pow(10, p1 + i * step);
     }
-
-    for (int i = 0; i < N_omega; i++) {
-        omega[i] = omega_min + i * dOmega;
+    else {
+        step = (right - left) / (N - 1);
+        for (int i = 0; i < N; i++)
+            mesh[i] = left + i * step;
     }
+}
 
-    for (int j = 0; j < N_e; j++) {
-        e[j] = e_min + j * de;
+void allocate2d(int N1, int N2, valarray<valarray<double> > & A) {
+    A.resize(N1);
+    for (int i = 0; i < N1; i++) {
+        A[i].resize(N2, 0.0);
     }
+}
+
+void MathieuEquation_Arnold() {
+    double omega_min, omega_max;
+    double a_min, a_max;
+    int N_omega, N_a;
+
+    inputMesh("omega", "a", &N_omega, &omega_min, &omega_max, &N_a, &a_min, &a_max);
+
+    valarray<double> omega, a;
+    createMesh(N_omega, omega_min, omega_max, omega);
+    createMesh(N_a, a_min, a_max, a);
+
+    valarray<valarray<double> > tr, det;
+    allocate2d(N_omega, N_a, tr);
+    allocate2d(N_omega, N_a, det);
+
 
     double T = 2.0 * M_PI;
     int N_t = 6000;
@@ -186,9 +183,9 @@ void case1() {
         double A[2][2];
         #pragma omp for
         for (int i = 0; i < N_omega; i++) {
-            for (int j = 0; j < N_e; j++) {
+            for (int j = 0; j < N_a; j++) {
                 eqs.setParameter(0, omega[i]);
-                eqs.setParameter(1, e[j]);
+                eqs.setParameter(1, a[j]);
 
                 method.map(&eqs, 0.0, ic1, img1, N_t, T);
                 method.map(&eqs, 0.0, ic2, img2, N_t, T);
@@ -202,43 +199,30 @@ void case1() {
             
             #pragma omp critical
             {
-                count += N_e;
-                printf("Ready %6d from %6d\n", count, N_omega * N_e);
+                count += N_a;
+                printf("Ready %6d from %6d\n", count, N_omega * N_a);
             }
         }
     }
     printf("Time: %lg\n", omp_get_wtime() - start);
-    saveToFile("trace.csv", omega, e, tr);
-    saveToFile("det.csv", omega, e, det);
+    saveToFile("trace.csv", omega, a, tr);
+    saveToFile("det.csv", omega, a, det);
 }
 
-void case2() {
-    double omega_min = -0.5;
-    double omega_max = 2.5;
-    double e_min = 0.0;
-    double e_max = 1.0;
-
-    int N_omega = 1001;
-    int N_e = 1001;
+void MathieuEquation_Kovacic() {
+    double delta_min, delta_max;
+    double epsilon_min, epsilon_max;
+    int N_delta, N_epsilon;
     
-    double dOmega = (omega_max - omega_min) / (N_omega - 1);
-    double de = (e_max - e_min) / (N_e - 1);
+    inputMesh("delta", "epsilon", &N_delta, &delta_min, &delta_max, &N_epsilon, &epsilon_min, &epsilon_max);
 
-    valarray<double> e(N_e);
-    valarray<double> omega(N_omega);
-    valarray<valarray<double> > tr(N_omega), det(N_omega);
-    for (int i = 0; i < N_omega; i++) {
-        tr[i].resize(N_e, 0.0);
-        det[i].resize(N_omega, 0.0);
-    }
+    valarray<double> delta, epsilon;
+    createMesh(N_delta, delta_min, delta_max, delta);
+    createMesh(N_epsilon, epsilon_min, epsilon_max, epsilon);
 
-    for (int i = 0; i < N_omega; i++) {
-        omega[i] = omega_min + i * dOmega;
-    }
-
-    for (int j = 0; j < N_e; j++) {
-        e[j] = e_min + j * de;
-    }
+    valarray<valarray<double> > tr, det;
+    allocate2d(N_delta, N_epsilon, tr);
+    allocate2d(N_delta, N_epsilon, det);
 
     double T = 2.0 * M_PI;
     int N_t = 6000;
@@ -257,10 +241,10 @@ void case2() {
         double img1[2], img2[2];
         double A[2][2];
         #pragma omp for
-        for (int i = 0; i < N_omega; i++) {
-            for (int j = 0; j < N_e; j++) {
-                eqs.setParameter(0, omega[i]);
-                eqs.setParameter(1, e[j]);
+        for (int i = 0; i < N_delta; i++) {
+            for (int j = 0; j < N_epsilon; j++) {
+                eqs.setParameter(0, delta[i]);
+                eqs.setParameter(1, epsilon[j]);
 
                 method.map(&eqs, 0.0, ic1, img1, N_t, T);
                 method.map(&eqs, 0.0, ic2, img2, N_t, T);
@@ -274,127 +258,36 @@ void case2() {
             
             #pragma omp critical
             {
-                count += N_e;
-                printf("Ready %6d from %6d\n", count, N_omega * N_e);
+                count += N_epsilon;
+                printf("Ready %6d from %6d\n", count, N_delta * N_epsilon);
             }
         }
     }
     printf("Time: %lg\n", omp_get_wtime() - start);
-    saveToFile("trace.csv", omega, e, tr);
-    saveToFile("det.csv", omega, e, det);
+    saveToFile("trace.csv", delta, epsilon, tr);
+    saveToFile("det.csv", delta, epsilon, det);
 }
 
-void case3() {
 
-    double omega_min = 0.0;
-    double omega_max = 6.0;
-    double dJ2_min = 0.0;
-    double dJ2_max = 2.0;
 
-    int N_omega = 501;
-    int N_dJ2 = 501;
-    
-    double dOmega = (omega_max - omega_min) / (N_omega - 1);
-    double ddJ2 = (dJ2_max - dJ2_min) / (N_dJ2 - 1);
-
-    valarray<double> dJ2(N_dJ2);
-    valarray<double> omega(N_omega);
-    valarray<valarray<double> > tr(N_omega), det(N_omega);
-    for (int i = 0; i < N_omega; i++) {
-        tr[i].resize(N_dJ2, 0.0);
-        det[i].resize(N_omega, 0.0);
-    }
-
-    for (int i = 0; i < N_omega; i++) {
-        omega[i] = omega_min + i * dOmega;
-    }
-
-    for (int j = 0; j < N_dJ2; j++) {
-        dJ2[j] = dJ2_min + j * ddJ2;
-    }
-
-    double T = 1.0;
-    int N_t = 10000;
-    printf("Integration Step: %lg\n", T / N_t);
-    double ic1[2] = { 1.0, 0.0 }, ic2[2] = { 0.0, 1.0 };
-   
-    int count = 0;
-    
-    double start = omp_get_wtime();
-    #pragma omp parallel
-    {
-        RungeKutta method;
-        LiouvilleHill eqs;
-        method.init(&eqs);
-        eqs.setParameter(0, 2.0);
-        eqs.setParameter(1, 3.0);
-        eqs.setParameter(2, 4.0);
-
-        double img1[2], img2[2];
-        double A[2][2];
-        #pragma omp for
-        for (int i = 0; i < N_omega; i++) {
-            for (int j = 0; j < N_dJ2; j++) {
-                eqs.setParameter(3, dJ2[j]);
-                eqs.setParameter(4, omega[i]);
-
-                method.map(&eqs, 0.0, ic1, img1, N_t, T);
-                method.map(&eqs, 0.0, ic2, img2, N_t, T);
-            
-                A[0][0] = img1[0]; A[0][1] = img2[0]; 
-                A[1][0] = img1[1]; A[1][1] = img2[1];
-
-                det[i][j] = A[0][0] * A[1][1] - A[1][0] * A[0][1];
-                tr[i][j] = fabs(A[0][0] + A[1][1]);
-            }
-            
-            #pragma omp critical
-            {
-                count += N_dJ2;
-                printf("Ready %6d from %6d\n", count, N_omega * N_dJ2);
-            }
-        }
-    }
-    printf("Time: %lg\n", omp_get_wtime() - start);
-    saveToFile("trace.csv", omega, dJ2, tr);
-    saveToFile("det.csv", omega, dJ2, det);
-}
-
-void case4() {
+void EulerPoinsotStability() {
     double OMEGA_N_CONST = 200.0 * M_PI;
 
-    double omega_min = 0.02;
-    double omega_max = 0.22;
-    double dJ2_min = 0.0;
-    double dJ2_max = 0.5;
+    double Omega_min, Omega_max;
+    double dJ2_min, dJ2_max;
+    int N_Omega, N_dJ2;
 
-    int N_omega = 16001;
-    int N_dJ2 = 1001;
-    
-    double ddJ2 = (dJ2_max - dJ2_min) / (N_dJ2 - 1);
+    inputMesh("Omega", "dJ2", &N_Omega, &Omega_min, &Omega_max, &N_dJ2, &dJ2_min, &dJ2_max);
 
-    valarray<double> dJ2(N_dJ2);
-    valarray<double> omega(N_omega);
-    valarray<valarray<double> > tr(N_omega), det(N_omega);
-    for (int i = 0; i < N_omega; i++) {
-        tr[i].resize(N_dJ2, 0.0);
-        det[i].resize(N_dJ2, 0.0);
-    }
+    valarray<double> Omega, dJ2;
+    createMesh(N_Omega, Omega_min, Omega_max, Omega, true);
+    createMesh(N_dJ2, dJ2_min, dJ2_max, dJ2);
 
-    double p1 = log10(omega_min);
-    double p2 = log10(omega_max);
-    //double dOmega = (omega_max - omega_min) / (N_omega - 1);
-    double dOmega = (p2 - p1) / (N_omega - 1);
-    for (int i = 0; i < N_omega; i++) {
-        omega[i] = pow(10, p1 + i * dOmega);
-    }
-
-    for (int j = 0; j < N_dJ2; j++) {
-        dJ2[j] = dJ2_min + j * ddJ2;
-    }
+    valarray<valarray<double> > tr, det;
+    allocate2d(N_Omega, N_dJ2, tr);
+    allocate2d(N_Omega, N_dJ2, det);
 
     double T = 1.0;
-    //printf("Integration Step: %lg\n", T / N_t);
     double ic1[2] = { 1.0, 0.0 }, ic2[2] = { 0.0, 1.0 };
    
     int count = 0;
@@ -413,12 +306,12 @@ void case4() {
         double img1[2], img2[2];
         double A[2][2];
         #pragma omp for schedule(dynamic, 1)
-        for (int i = 0; i < N_omega; i++) {
+        for (int i = 0; i < N_Omega; i++) {
             for (int j = 0; j < N_dJ2; j++) {
                 eqs.setParameter(3, dJ2[j]);
-                eqs.setParameter(4, omega[i]);
+                eqs.setParameter(4, Omega[i]);
 
-                int N_t = (int)ceil(OMEGA_N_CONST / omega[i]);
+                int N_t = (int)ceil(OMEGA_N_CONST / Omega[i]);
 
                 method.map(&eqs, 0.0, ic1, img1, N_t, T);
                 method.map(&eqs, 0.0, ic2, img2, N_t, T);
@@ -433,11 +326,11 @@ void case4() {
             #pragma omp critical
             {
                 count += N_dJ2;
-                printf("Ready %6d from %6d\n", count, N_omega * N_dJ2);
+                printf("Ready %6d from %6d\n", count, N_Omega * N_dJ2);
             }
         }
     }
     printf("Time: %lg\n", omp_get_wtime() - start);
-    saveToFile("trace.csv", omega, dJ2, tr);
-    saveToFile("det.csv", omega, dJ2, det);
+    saveToFile("trace.csv", Omega, dJ2, tr);
+    saveToFile("det.csv", Omega, dJ2, det);
 }
